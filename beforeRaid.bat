60dcf1e0864d97b7677ba13f309776f497c25686
MrLBRD <58847864+MrLBRD@users.noreply.github.com>
2022-05-02 20:49:14 +0200

    Add bat for open all browser

---
+++ beforeRaid.bat
@@ -0,0 +1,13 @@
+@echo off
+set /p USERNAME="Username : "
+echo Your name is: %USERNAME%
+
+start chrome "https://www.twitch.tv/%USERNAME%"
+start firefox.exe "https://www.twitch.tv/%USERNAME%"
+start opera.exe "https://www.twitch.tv/%USERNAME%"
+start msedge.exe "https://www.twitch.tv/%USERNAME%"
+start brave.exe "https://www.twitch.tv/%USERNAME%"
+start vivaldi.exe "https://www.twitch.tv/%USERNAME%"
+start urbrowser.exe "https://www.twitch.tv/%USERNAME%"
+start seamonkey.exe "https://www.twitch.tv/%USERNAME%"
+start Maxthon.exe "https://www.twitch.tv/%USERNAME%"
